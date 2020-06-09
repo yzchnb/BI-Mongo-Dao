7423ef7a722bf6ada3aabf645ff101b6ae2a1ccb
yzchnb <1021777674@qq.com>
2020-06-09 08:46:05 +0800

    add config

--- src/main/java/com/yzchnb/bimdbdao/BiMdbDaoApplication.java
+++ src/main/java/com/yzchnb/bimdbdao/BiMdbDaoApplication.java
@@ -2,11 +2,13 @@ package com.yzchnb.bimdbdao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class BiMdbDaoApplication {
 
